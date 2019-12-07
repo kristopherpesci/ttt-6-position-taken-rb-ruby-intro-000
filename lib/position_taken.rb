404ee10788e1435264bffe2b2b6ff15f8f43378f
kristopherpesci <kristopher.pesci@gmail.com>
2019-12-07 22:24:42 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,7 @@
 def position_taken?(board,index)
   if board[index] == " " || "" || nil
     false
-  else board[index] != " " || "" || nil
+  else board[index] == "X" || "O"
     true
     end
   end
