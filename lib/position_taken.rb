c09e898148a43d88790ded5a69d16feba12d087d
kristopherpesci <kristopher.pesci@gmail.com>
2019-12-07 21:48:31 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,13 @@
 # code your #position_taken? method here!
 
-def position_taken?
+def position_taken?  array[index_number]
+  if array[index_number] == " "
+    "false"
+  end
+if-else array[index_number] == ""
+  "false"
+  end
+else
+  "true"
 end
+  end
