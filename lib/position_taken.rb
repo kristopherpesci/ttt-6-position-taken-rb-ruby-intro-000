f5c92bfb894c4ffcf84bd49d1e5c709d0ecb6bea
kristopherpesci <kristopher.pesci@gmail.com>
2019-12-07 22:35:16 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,8 +2,8 @@
 
 def position_taken?(board,index)
     if (board[index] ==  " " || board[index] == "" || board[index] == nil)
-    taken = false
+    false
     else
-    taken = true
+    true
     end
   end
