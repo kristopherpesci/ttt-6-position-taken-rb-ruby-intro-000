9383c14cd9426ed54193ad2344a7b7e7070e4bfd
kristopherpesci <kristopher.pesci@gmail.com>
2019-12-07 22:04:26 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 
 def position_taken?(board,index_number)
-  if board[index_number] == " "
-    print "false"
-  end
+board[index_number] == " "
+"false"
+
   end
